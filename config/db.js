4039e3a24e4d7b7f6b91d1436d1bea60d794224d
Phucnguyen <nguyen.phuc@jvb-corp.com>
2023-02-13 00:41:33 +0700

    Add mongoDb

---
+++ config/db.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+// eslint-disable-next-line prefer-destructuring
+const MONGO_URI = process.env.MONGO_URI;
+console.log('process.env.MONGO_URI', MONGO_URI);
+
+const connectDb = async () => {
+  try {
+    const conn = await mongoose.connect(MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(`Error: ${err.message}`);
+    process.exit(1);
+  }
+};
+module.exports = { connectDb };
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 
 const express = require('express')
+const morgan = require('morgan')
+const connectDb = require('./config/db.js');
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+connectDb.connectDb()
+app.use(morgan('combined'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
