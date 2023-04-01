fb437ae0fbc4cbc86def8833999dcb452c1b1a62
Phucnguyen <nguyen.phuc@jvb-corp.com>
2023-04-02 02:12:57 +0700

    Commit 1-4

--- routes/newsRoutes.js
+++ routes/newsRoutes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const {
   handleGetNews,
   handleGetNewsId,
-  handleFavorite
+  handleFavorite,
+  handleCreateNews
 } = require("../controllers/news/newsController");
 
 const routerNews = express.Router();
@@ -10,5 +11,6 @@ const routerNews = express.Router();
 routerNews.get("/all", handleGetNews);
 routerNews.get("/:id", handleGetNewsId);
 routerNews.post("/favorite", handleFavorite);
+routerNews.post("/create", handleCreateNews);
 
 module.exports = routerNews;
