7b2affc8eeebce80b24cc3bcb283398cb9156c5e
phucntp <phucqaz.1234@gmail.com>
2023-11-19 14:42:07 +0700

    Fix bug cross

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
   "https://clone-tiktok-fe-phucntp.vercel.app",
   "https://clone-api-tiktok.onrender.com",
+  "https://clone-tiktok-be.vercel.app",
   "http://localhost:2000",
   "http://localhost:3000",
   "https://clone-tiktok-fe-git-master-phucntp.vercel.app"
